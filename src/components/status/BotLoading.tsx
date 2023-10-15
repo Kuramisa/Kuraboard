2a90d9ec28d4079bc24f2f1e154f261aed24019d
mateie <mate@gmail.com>
2023-10-14 17:56:22 -0700

    Fixed a crashing bug

--- src/components/status/BotLoading.tsx
+++ src/components/status/BotLoading.tsx
@@ -5,7 +5,7 @@ const BotLoading = () => {
         <div className="flex flex-column justify-content-center align-items-center" style={{height: "90vh"}}>
             <img alt="Loading" src="/logo.png" style={{width: "96px"}}/>
             <ProgressSpinner style={{width: "128px", height: "128px", position: "absolute", top: "265px"}}
-                             strokeWidth={2}
+                             strokeWidth="2"
                              animationDuration="3s"/>
             <h2>Kuramisa</h2>
             <h3>Loading...</h3>
