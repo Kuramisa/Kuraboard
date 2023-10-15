d9de6079c778ed77679c33fc90a0740030e6748a
mateie <mate@gmail.com>
2023-10-14 18:01:51 -0700

    Fixed weird bot loading placement

--- src/components/status/BotLoading.tsx
+++ src/components/status/BotLoading.tsx
@@ -3,10 +3,11 @@ import {ProgressSpinner} from "primereact/progressspinner";
 const BotLoading = () => {
     return (
         <div className="flex flex-column justify-content-center align-items-center" style={{height: "90vh"}}>
-            <img alt="Loading" src="/logo.png" style={{width: "96px"}}/>
-            <ProgressSpinner style={{width: "128px", height: "128px", position: "absolute", top: "265px"}}
+
+            <ProgressSpinner style={{width: "128px", height: "128px"}}
                              strokeWidth="2"
                              animationDuration="3s"/>
+            <img alt="Loading" src="/logo.png" style={{width: "96px", position: "absolute", top: "33vh"}}/>
             <h2>Kuramisa</h2>
             <h3>Loading...</h3>
         </div>
