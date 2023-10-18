a3e4bc2736b20818c727c68b91e119a945491932
âMateie <mate@gmail.com>
2023-10-17 20:17:36 -0700

    Moved navigation redirection for the hook

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,21 @@
 import {useContext} from "react";
+import {Buffer} from "buffer";
 import {AuthContext} from "../providers/AuthProvider.tsx";
 import {Navigate} from "react-router-dom";
 import {useQuery} from "@apollo/client";
-import {LoginUser} from "../gql/auth.tsx";
+import {LoginUser} from "../gql/auth";
+
 
 const Login = () => {
     const {auth, login} = useContext(AuthContext);
-
-    if (auth) return <Navigate to="/" replace={true}/>;
 
     const code = window.location.search.split("=")[1];
 
     const {data, error} = useQuery(LoginUser, {
         variables: {code: Buffer.from(code).toString("base64")},
     });
+
+    if (auth) return <Navigate to="/" replace={true}/>;
 
     if (error) return <Navigate to="/" replace={true}/>;
 
