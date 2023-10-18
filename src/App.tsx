794eb01fd9c28f9d3e252f777261639a3d9731f7
âMateie <mate@gmail.com>
2023-10-17 20:18:25 -0700

    Added navigation and routes to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import {useQuery} from "@apollo/client";
-import {FetchClient} from "./gql/client.tsx";
+import {FetchClientUser} from "./gql/client";
 
 import {UnderDevelopment, BotLoading, BotOffline} from "./components/status";
+import Navigation from "./components/Navigation";
+import Login from "./pages/Login";
+import {Route, Routes} from "react-router-dom";
 
 const {VITE_UNDER_DEV} = import.meta.env;
 
 const App = () => {
-    const {loading, error: botError} = useQuery(FetchClient, {
+    const {loading, error} = useQuery(FetchClientUser, {
         pollInterval: 100000
     });
 
     if (VITE_UNDER_DEV === "true") return <UnderDevelopment/>;
 
     if (loading) return <BotLoading/>;
-    if (botError) return <BotOffline/>;
+    if (error) return <BotOffline/>;
 
     return (
-        <></>
+        <div className="block">
+            <Navigation/>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </div>
     );
 };
 
