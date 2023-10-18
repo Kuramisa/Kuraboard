966e87c290f625f47ce02d9f979ac6b8a4f2180d
âMateie <mate@gmail.com>
2023-10-17 20:16:49 -0700

    Added a type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="vite/client" />
+
+export type Bot = {
+    accentColor?: string;
+    avatar?: string;
+    avatarDecoration?: string;
+    avatarURL?: string;
+    banner?: string;
+    bot: boolean;
+    description?: string;
+    discriminator: string;
+    flags: number;
+    globalName?: string;
+    guilds: number;
+    id: string;
+    mfaEnabled: boolean;
+    system: boolean;
+    username: string;
+    users: number;
+    verified: boolean;
+}
