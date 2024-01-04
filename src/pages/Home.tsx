1d589972b1b8e597e8920a464e9c4d49d514c3f6
mateie <mateie@gmail.com>
2024-01-04 13:00:51 -0800

    Centered the home page and escaped entities

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
 const Home = () => {
 
     return (
-        <>
+        <div className="flex align-content-center justify-content-center">
             <h1>
-                I know, I know. It's empty 😿
+                I know, I know. It&quot;s empty 😿
             </h1>
             <h2>
-                I'm working on it, I promise!
+                I&qout;m working on it, I promise!
             </h2>
-        </>
+        </div>
     );
 };
 
