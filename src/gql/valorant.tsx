ad53667d38126cf8e4a1d51e153bf1edf449606b
mateie <mateie@gmail.com>
2024-01-09 05:04:29 -0800

    Added api requests gql on frontend

--- src/gql/valorant.tsx
+++ src/gql/valorant.tsx
@@ -5,3 +5,9 @@ export const FetchValorantWeapons = gql`
         weapons
     }    
 `;
+
+export const FetchValorantWeapon = gql`
+    query weapon($weaponUuid: String!, $withSkins: Boolean = false) {
+        weapon(weaponUuid: $weaponUuid, withSkins: $withSkins)
+    }  
+`;
